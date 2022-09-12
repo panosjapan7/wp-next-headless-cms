521501b8da32179fa92b5735ab5a59db37097f63
panosjapan7 <panosjapan@gmail.com>
2022-09-12 13:14:54 +0200

    Creates category page

---
+++ pages/[category].js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import Link from "next/link";
+import { useRouter } from 'next/router';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/category.module.css'
+
+const Category = () => {
+    const router = useRouter();
+    const { category } = router.query;
+    const [posts, setPosts] = useState([]);
+  
+    const fetchData = async () => {
+        console.log("category: ", category);
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/posts?category=${category}`
+      );
+      const data = await response.json();
+      setPosts(data.posts);
+      console.log("data.posts: ", data.posts)
+    };
+  
+    useEffect(() => {
+      fetchData();
+      // eslint-disable-next-line
+    }, [category]);
+  
+    return (
+      <div className={styles.categoryPageWrapper}>
+        <p className={styles.categoryName}>{category}</p>
+        <div className={styles.postsList}>
+          {posts &&
+            posts.map((post, index) => {
+              return (
+                <Link href={`/posts/${post.ID}`} key={index}>
+                  <p className={styles.postTitle}>{post.title}</p>
+                </Link>
+              );
+            })}
+        </div>
+      </div>
+    );
+  };
+  
+  export default Category;
