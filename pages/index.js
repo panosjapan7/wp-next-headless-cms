9dd16927e9fb953a0ecd4a9a21ee4b23cd36f7d1
panosjapan7 <panosjapan@gmail.com>
2022-09-12 12:40:44 +0200

    Adds title and head to home page

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,14 @@ function Home() {
   }, []);
 
   return (
-
+    <>
+    <Head>
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="title, meta, nextjs" />
+        <meta name="author" content="Syamlal CM" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Webb21 CMS Examinationsuppgift</title>
+      </Head>
       <div className={styles.home}>
         <p className={styles.recentPosts}>All Posts</p>
         {posts &&
@@ -35,6 +42,7 @@ function Home() {
             );
           })}
       </div>
+    </>
 
   );
 }
