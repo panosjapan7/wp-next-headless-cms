a861659316258ef5971dc124069a85ba5137f356
panosjapan7 <panosjapan@gmail.com>
2022-09-12 11:06:01 +0200

    adds navbar, categories bar to home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import CategoryList from "../components/categoryList";
+import NavBar from "../components/navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar></NavBar>
+      <CategoryList></CategoryList>
+      <Component {...pageProps} />
+    </>
+  
+  )
 }
 
 export default MyApp
